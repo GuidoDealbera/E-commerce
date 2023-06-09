6d25f0bc329bbfd57d59cf3439a42faa96a3a55e
GuidoDealbera <guidojdealbera@gmail.com>
2023-06-08 21:27:44 -0300

    Creacion del modelo USER

---
+++ api/src/Models/User.ts
@@ -0,0 +1,52 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize: any) => {
+    sequelize.define(
+        "User",
+        {
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                defaultValue: DataTypes.UUIDV4,
+            },
+            name: {
+                type: DataTypes.STRING,
+            },
+            lastName: {
+                type: DataTypes.STRING,
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            profilePhoto: {
+                type: DataTypes.STRING,
+            },
+            phone: {
+                type: DataTypes.STRING,
+            },
+            address: {
+                type: DataTypes.STRING,
+            },
+            postalCode: {
+                type: DataTypes.STRING,
+            },
+            interests: {
+                type: DataTypes.ARRAY,
+            },
+            seller: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false,
+            },
+        }
+    )
+}
