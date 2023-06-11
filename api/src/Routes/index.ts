b83517022f9249c2339cfe54d30339708bcc4c9e
GuidoDealbera <guidojdealbera@gmail.com>
2023-06-11 19:39:04 -0300

    getAllReviews

--- api/src/Routes/index.ts
+++ api/src/Routes/index.ts
@@ -7,15 +7,16 @@ import postProduct from "../Controllers/Products/postProduct";
 import getUserById from "../Controllers/Users/getUserById";
 
 import postReview from "../Controllers/Reviews/postReviews";
+import getAllReviews from "../Controllers/Reviews/getAllReviews";
 
 
+router.get("/reviews", getAllReviews);
 router.get("/user/:id", getUserById)
 router.get("/getAllProducts",getAllProducts)
 router.post("/postProduct", postProduct)
 router.post("/postUser", postUser)
 
 router.post("/postReview", postReview);
-
 
 
 export default router;
