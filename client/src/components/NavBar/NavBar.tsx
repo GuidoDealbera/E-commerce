b3588e94d20029528e7076d134830110d9a0ca14
Luis <lcbtrejo1206@gmail.com>
2023-06-21 21:17:19 -0400

    estilos de navbar y data.ts

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar(){
     return(
-        <div className="nav-bg">
+        <div className="navbar">
             <h1>hola soy la navbar</h1>
             <button>Catálogo</button>
             <button className="button1">Login</button>
