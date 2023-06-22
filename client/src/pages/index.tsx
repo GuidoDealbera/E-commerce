785fa036ec8919b248804613db5e8c217fd86404
Luis <lcbtrejo1206@gmail.com>
2023-06-22 19:13:26 -0400

    cartitas

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import NavBar from "@/components/NavBar/NavBar"
+import Cards from "@/components/Cards/Cards"
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
-
+      <Cards/>
     </div>
   )
 }
