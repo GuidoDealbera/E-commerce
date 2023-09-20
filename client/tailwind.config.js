2f8086b6eee09f7abb676f5bc642c8eeb045ecc1
GuidoDealbera <guidojdealbera@gmail.com>
2023-09-20 19:00:48 -0300

    Creacion de componentes y estructura de carpetas

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/Components/**/*.{js,ts,jsx,tsx}",
+    "./src/Hooks/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+}
+
