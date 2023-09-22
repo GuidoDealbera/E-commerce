9008fb2207f489196aff58137a691982744cba66
GuidoDealbera <guidojdealbera@gmail.com>
2023-09-22 03:13:38 -0300

    LoginForm Style

--- client/src/Components/Forms/LoginForm.tsx
+++ client/src/Components/Forms/LoginForm.tsx
@@ -1,35 +1,38 @@
 import {FcGoogle} from 'react-icons/fc'
-import {FaSquareFacebook} from 'react-icons/fa6'
+import {FaSquareFacebook, FaRegEyeSlash, FaEye} from 'react-icons/fa6';
 import { useForms } from '../../Hooks/useForms';
+import { useState } from 'react';
 
 const LoginForm: React.FC = () => {
+    const [showPassword, setShowPassword] = useState<boolean>(false)
     const {googleSession, facebookSession} = useForms();
     return (
     <div className='flex py-10 justify-center items-center'>
         <form className='flex flex-col justify-center items-center gap-2'>
-            <h1>Ingresa tu cuenta</h1>
-            <div>
-                <input type="email" name="email"/>
+            <h1 className='text-2xl uppercase mb-5'>Ingresa tu cuenta</h1>
+            <div className='inputs mb-5'>
+                <input type="email" name="email" className='' required/>
                 <label>Correo Electrónico</label>
             </div>
-            <div>
-                <input type="password" name="password" />
+            <div className='inputs mb-5'>
+                <input type={showPassword ? "text" : "password"} name="password" required/>
                 <label>Contraseña</label>
+                <button type='button' onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaRegEyeSlash/> : <FaEye/>}</button>
             </div>
-            <button type="submit">Iniciar Sesión</button>
+            <button type="submit" className='bg-blue-700 w-full p-2 rounded-lg hover:bg-blue-800 duration-200'>Iniciar Sesión</button>
         <h1>Ó</h1>
       <div className='w-full flex flex-col gap-2'>
-        <button className='flex flex-row items-center relative gap-2 w-full py-1 hover:bg-red-700 hover:text-white duration-300 rounded-lg bg-white text-black font-medium justify-center' onClick={googleSession}>
+        <button className='flex flex-row items-center relative gap-2 w-full py-2 bg-red-800 hover:bg-red-700 text-white duration-300 rounded-lg font-medium justify-center' onClick={googleSession}>
             <span className='bg-white text-3xl absolute left-2 rounded-lg'>
             <FcGoogle/>
             </span>
-            <span className=''>Iniciar con Google</span>
+            <span className=''>Inicia con Google</span>
         </button>
-        <button className='flex flex-row items-center relative gap-2 w-full py-1 hover:bg-red-700 hover:text-white duration-300 rounded-lg bg-white text-black font-medium justify-center' onClick={facebookSession}>
-        <span className='bg-white text-3xl absolute left-2 rounded-lg'>
+        <button className='flex flex-row items-center relative gap-2 w-full py-2 bg-blue-950 hover:bg-blue-900 text-white duration-300 rounded-lg font-medium justify-center' onClick={facebookSession}>
+        <span className='text-3xl text-white absolute left-2 rounded-lg'>
             <FaSquareFacebook/>
             </span>
-            <span>Iniciar con Facebook</span>
+            <span>Inicia con Facebook</span>
         </button>
       </div>
         </form>
