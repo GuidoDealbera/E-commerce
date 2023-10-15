8dc3b8efa812340e422fcf1f59feb39fab7b83d1
GuidoDealbera <guidojdealbera@gmail.com>
2023-10-15 05:07:57 -0300

    passport middleware

--- api/src/Middlewares/passportMiddleware.ts
+++ api/src/Middlewares/passportMiddleware.ts
@@ -1,27 +1,23 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
-import { DataBase } from '../db';
-const {User} = DataBase.conn.models;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BACKEND_URL } = process.env;
+import { DataBase } from "../db";
+const { User } = DataBase.conn.models;
 import passport from "passport";
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as FacebookStrategy } from "passport-facebook";
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: `${GOOGLE_CLIENT_ID}`,
       clientSecret: `${GOOGLE_CLIENT_SECRET}`,
-      callbackURL: "http://localhost:3001/auth/google/callback",
+      callbackURL: `${BACKEND_URL}/auth/google/callback`,
     },
-    async function (
-      accessToken: string,
-      refreshToken: string,
-      profile: any,
-      done: any
-    ) {
+    async (accessToken, refreshToken, profile, done) => {
       try {
-        let user = await User.findOne({where:{ googleId: profile.id }});
-        if(user) {
+        let user = await User.findOne({ where: { googleId: profile.id } });
+        if (user) {
           return done(null, user);
         } else {
           const fullName = profile.displayName.split(" ");
@@ -38,9 +34,68 @@ passport.use(
           return done(null, user);
         }
       } catch (error) {
-        console.log(error);
-        return error;
+        console.error(error);
+        if (typeof error === "string" || error instanceof Error) {
+          return done(error);
+        }
       }
     }
   )
 );
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "Esta variable debe ser de entorno",
+      clientSecret: "Esta variable debe ser de entorno",
+      callbackURL: "Esta variable debe ser de entorno",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ where: { facebookId: profile.id } });
+        if (user) {
+          return done(null, user);
+        } else {
+          const fullName = profile.displayName.split(" ");
+          const name = fullName[0];
+          const lastName = fullName[1];
+          const username = fullName + profile.id;
+          const newUser = {
+            facebookId: profile.id,
+            name,
+            lastName,
+            username,
+          };
+          user = await User.create(newUser);
+          return done(null, user);
+        }
+      } catch (error) {
+        console.error(error);
+        if (typeof error === "string" || error instanceof Error) {
+          return done(error);
+        }
+      }
+    }
+  )
+);
+
+passport.serializeUser(async (user, done) => {
+  try {
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await User.findOne({ where: { id: id } });
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, null);
+    }
+  } catch (error) {
+    done(error);
+  }
+});
