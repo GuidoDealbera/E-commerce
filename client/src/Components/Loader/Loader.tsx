6a5e03c2bd1a3e9c21db2f6c02416142a9abfa61
GuidoDealbera <guidojdealbera@gmail.com>
2023-10-19 23:08:41 -0300

    fix loader

--- client/src/Components/Loader/Loader.tsx
+++ client/src/Components/Loader/Loader.tsx
@@ -1,14 +1,20 @@
 interface Props {
-    isLoading: boolean;
+  isLoading: boolean;
 }
-const Loader: React.FC<Props> = ({isLoading}) => {
-    return (
-        <div className={`${!isLoading && "hidden"} absolute flex justify-center items-center min-h-screen w-screen bg-gray-900/80 backdrop-blur-sm`}>
-            <span className="loader">
-                
-            </span>
-        </div>
-    )
+const Loader: React.FC<Props> = ({ isLoading }) => {
+  const path = location.pathname;
+  return (
+    <div
+      className={`${
+        !isLoading && "hidden"
+      } absolute flex flex-col gap-4 justify-center items-center min-h-screen w-screen bg-gray-900/80 backdrop-blur-sm`}
+    >
+      <h1 className="text-3xl">
+        {path === "/auth/callback" ? "CARGANDO DATOS DE USUARIO" : "CARGANDO"}
+      </h1>
+      <span className="loader"></span>
+    </div>
+  );
 };
 
 export default Loader;
